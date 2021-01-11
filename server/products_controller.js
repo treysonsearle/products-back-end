f143b5575fa9b562915aec047c9f3aab16a9e903
treysonsearle <treyson.searle96@gmail.com>
2021-01-11 16:31:52 -0700

    i hope i am not uploading node modules

---
+++ server/products_controller.js
@@ -0,0 +1,60 @@
+module.exports = {
+  create: (req, res, next) => {
+    const db = req.app.get('db');
+    const { name, description, price, image_url } = req.body;
+
+    db.create_product([name, description, price, image_url])
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Its a trap!!!" });
+        console.log(err)
+      });
+  },
+
+  getOne: (req, res, next) => {
+    const db = req.app.get('db');
+    const { id } = req.params;
+
+    db.read_product(id)
+      .then(product => res.status(200).send(product))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Its a trap!!!" });
+        console.log(err)
+      });
+  },
+
+  getAll: (req, res, next) => {
+    const db = req.app.get('db');
+
+    db.read_products()
+      .then(products => res.status(200).send(products))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Its a trap!!!" });
+        console.log(err)
+      });
+  },
+
+  update: (req, res, next) => {
+    const db = req.app.get('db');
+    const { params, query } = req;
+
+    db.update_product([params.id, query.desc])
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Its a trap!!!" });
+        console.log(err)
+      });
+  },
+
+  delete: (req, res, next) => {
+    const db = req.app.get('db');
+    const { id } = req.params;
+
+    db.delete_product(id)
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Its a trap!!!" });
+        console.log(err)
+      });
+  }
+};
